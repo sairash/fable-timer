856af1b3efb699255cb56d4da2f4169bb293cbcf
Sai On Action <gautamsairash@gmail.com>
2025-05-27 01:04:49 +0545

    Custom modal component

---
+++ src/components/custom-ui/modal.tsx
@@ -0,0 +1,21 @@
+import {ReactNode} from "react";
+
+
+function modal({close, title, children}: {close: ()=>void, title: string, children: ReactNode}) {
+
+    return (
+        <>
+            <div className="fixed w-full px-2 h-full max-w-md max-h-100   left-1/2 top-1/2" style={{transform: `translate(-50%, -50%)`, zIndex: 1000,}}>
+                <div className="px-4 h-full w-full py-2 bg-white rounded flex flex-col">
+                    <b className="py-1">{title}</b>
+                    <div className="w-full h-full overflow-y-auto overflow-x-hidden">
+                        {children}
+                    </div>
+                </div>
+            </div>
+            <div onClick={close} className="fixed top-0 left-0 w-screen h-screen cursor-pointer" style={{zIndex: 999, backgroundColor: "rgba(0,0,0, 0.5)",}}></div>
+        </>
+    )
+}
+
+export default  modal;
