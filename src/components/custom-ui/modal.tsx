d3b7fa66b73be1aa9f4ab99d6f385fda0df46381
Sai On Action <gautamsairash@gmail.com>
2025-05-28 17:56:05 +0545

    modal added to have headers aswell

--- src/components/custom-ui/modal.tsx
+++ src/components/custom-ui/modal.tsx
@@ -1,31 +1,41 @@
 import {ReactNode, useCallback, useEffect} from "react";
 
 
-function modal({close, title, children}: {close: ()=>void, title: string, children: ReactNode}) {
+
+interface ModalProps {
+    close: () => void;
+    title: string;
+    headerContent: ReactNode; 
+    bodyContent: ReactNode; 
+}
+
+
+function modal({close, title, headerContent, bodyContent}: ModalProps) {
 
     const handleKeyDown = useCallback((event: KeyboardEvent) => {
         if (event.key === 'Escape') {
             close();
         }
-    }, []);
+    }, [close]);
 
-    useEffect(()=>{
-
-       document.addEventListener('keydown', handleKeyDown);
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
 
         return () => {
             document.removeEventListener('keydown', handleKeyDown);
         }
-
-    }, [])
+    }, [handleKeyDown]);
 
     return (
         <>
             <div className="fixed w-full px-2 h-full max-w-md max-h-100   left-1/2 top-1/2" style={{transform: `translate(-50%, -50%)`, zIndex: 1000,}}>
                 <div className="px-4 h-full w-full py-2 bg-white rounded flex flex-col">
-                    <b className="py-1">{title}</b>
+                    <div className="flex justify-between pt-1 pb-2">
+                        <b className="">{title}</b>
+                        {headerContent}
+                    </div>
                     <div className="w-full h-full overflow-y-auto overflow-x-hidden">
-                        {children}
+                        {bodyContent}
                     </div>
                 </div>
             </div>
